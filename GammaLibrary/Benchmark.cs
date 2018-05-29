213e7c76faf1d2fc66819df022e03d1326bba229
Cyl18 <cyl18a@gmail.com>
2018-05-29 18:57:32 +0800

    add Benchmark

---
+++ GammaLibrary/Benchmark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace GammaLibrary
+{
+    public static class Benchmark
+    {
+        public static void MeasureTime(Action action, Action<TimeSpan> callback)
+        {
+            var sw = Stopwatch.StartNew();
+            action();
+            callback(sw.Elapsed);
+        }
+    }
+}
