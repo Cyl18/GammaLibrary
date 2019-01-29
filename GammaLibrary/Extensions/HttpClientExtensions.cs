87a39c620c3a9788ebd7b14fd21cf928fd218efb
Cyl18 <cyl18a@gmail.com>
2019-01-29 19:20:39 +0800

    add httpclient support

---
+++ GammaLibrary/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GammaLibrary.Extensions
+{
+    public static class HttpClientExtensions
+    {
+
+        public static async Task DownloadAsync(this HttpClient client, string requestUri, string destination,
+            IProgress<double> progress = default, CancellationToken cancellationToken = default)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(destination));
+            using (var fs = File.OpenWrite(destination))
+            {
+                await client.DownloadAsync(requestUri, fs, progress, cancellationToken);
+            }
+        }
+
+
+        public static async Task DownloadAsync(this HttpClient client, string requestUri, Stream destination,
+            IProgress<double> progress = default, CancellationToken cancellationToken = default)
+        {
+            using (var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead))
+            {
+                var contentLength = response.Content.Headers.ContentLength;
+                response.EnsureSuccessStatusCode();
+
+                using (var download = await response.Content.ReadAsStreamAsync())
+                {
+                    if (progress == null || !contentLength.HasValue)
+                    {
+                        await download.CopyToAsync(destination);
+                        return;
+                    }
+
+                    var relativeProgress = new Progress<long>(totalBytes => progress.Report((float)totalBytes / contentLength.Value));
+                    await download.CopyToAsync(destination, 81920, relativeProgress, cancellationToken);
+                    progress.Report(1);
+                }
+            }
+        }
+
+        public static async Task CopyToAsync(this Stream source, Stream destination, int bufferSize = 8192, IProgress<long> progress = null, CancellationToken cancellationToken = default)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (!source.CanRead)
+                throw new ArgumentException("Has to be readable", nameof(source));
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
+            if (!destination.CanWrite)
+                throw new ArgumentException("Has to be writable", nameof(destination));
+            if (bufferSize < 0)
+                throw new ArgumentOutOfRangeException(nameof(bufferSize));
+
+            var buffer = new byte[bufferSize];
+            long totalBytesRead = 0;
+            int bytesRead;
+            while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
+            {
+                await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+                totalBytesRead += bytesRead;
+                progress?.Report(totalBytesRead);
+            }
+        }
+    }
+}
