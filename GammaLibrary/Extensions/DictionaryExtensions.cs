8792d8920a2a40835ef0bf311044fec0a8d63008
Cyl18 <cyl18a@gmail.com>
2019-02-11 02:24:02 +0800

    yep

---
+++ GammaLibrary/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GammaLibrary.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static bool TryGet<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, out TValue value) 
+        {
+            var flag = dictionary.ContainsKey(key);
+            value = flag ? dictionary[key] : default;
+            return flag;
+        }
+    }
+}
